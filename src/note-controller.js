c302fcdacd865e3f3fe42ecff7831dc925af456e
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-09-15 17:03:40 +0100

    Added functionality to display notes on the index page

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,1 +1,8 @@
 document.getElementById('app').innerHTML = 'Howdy'
+
+var notelistview = new NoteListView();
+notelistview.notelist.addNote('Hello everyone!')
+notelistview.notelist.addNote('Its me again! :p')
+var listHTML = notelistview.html();
+console.log(listHTML)
+document.getElementById('note').innerHTML = listHTML
